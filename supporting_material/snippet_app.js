b250338be07b97b47d47f31535a6034022e38796
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-06-22 09:30:08 +0530

    Added supporting material for making the project.

---
+++ supporting_material/snippet_app.js
@@ -0,0 +1,23 @@
+// hard coded Array of location objects
+// https://github.com/udacity/ud864/blob/master/Project_Code_5_BeingStylish.html#L150
+
+// initMap function
+// https://developers.google.com/maps/documentation/javascript/examples/map-simple
+
+// Location constructor similiar to the Cat constructor form the JavaScript Design Patterns course (optional)
+
+// ViewModel constructor
+// In the ViewmModel create an observableArray with place objects
+// http://knockoutjs.com/documentation/observables.html#mvvm-and-view-models
+// Separating Out the Model video lesson:
+// https://classroom.udacity.com/nanodegrees/nd001/parts/e87c34bf-a9c0-415f-b007-c2c2d7eead73/modules/271165859175461/lessons/3406489055/concepts/34284402380923
+// Adding More Cats video lesson
+// https://classroom.udacity.com/nanodegrees/nd001/parts/e87c34bf-a9c0-415f-b007-c2c2d7eead73/modules/271165859175461/lessons/3406489055/concepts/34648186930923
+
+// Instantiate the ViewModel
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new
+// The difference between defining the ViewModel as a function expression or defining the viewModel as an object literal:
+// https://discussions.udacity.com/t/text-not-updating-with-search-box/182886/6
+
+// Apply the bindings aka activate KO
+// http://knockoutjs.com/documentation/observables.html#mvvm-and-view-models#activating-knockout 
