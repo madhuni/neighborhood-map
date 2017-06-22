bb6208795306a935ebc2bff088177074f5ee30ed
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-06-22 10:16:56 +0530

    Added the location array with the geomatrical details for locaions.

--- js/model.js
+++ js/model.js
@@ -1,22 +1,37 @@
 var locations = [
     {
-        title: '',
-        position: {lat: '', lng: ''}
+        title: 'Kota Kachori',
+        position: {lat: 12.936298, lng: 77.625429},
+        address: '635, 100 Feet Rd, 6th Block, Indiranagar, Bengaluru, Karnataka 560095, India',
+        placeId: 'ChIJL0R7XUMUrjsRvtH2I6WhCME'
     },
     {
-        title: '',
-        position: {lat: '', lng: ''}
+        title: 'Stoner',
+        position: {lat: 12.976307, lng: 77.64123099999999},
+        address: '100 Feet Rd, Defence Colony, Indiranagar, Bengaluru, Karnataka 560038, India',
+        placeId: 'ChIJmfKV7KUWrjsRPLIaeB5UMCw'
     },
     {
-        title: '',
-        position: {lat: '', lng: ''}
+        title: 'Matteo Coffea',
+        position: {lat: 12.9743821, lng: 77.6070038},
+        address: '2, Church Street, Shanthala Nagar, Ashok Nagar, Bengaluru, Karnataka 560001, India',
+        placeId: 'ChIJd1HW9X0WrjsRlH0QQRJMAwk'
     },
     {
-        title: '',
-        position: {lat: '', lng: ''}
+        
+        title: 'Wonderla Ammusement Park',
+        position: {lat: 12.834556, lng: 77.400972},
+        address: '28th km, Mysore Road, Bengaluru, Karnataka 562109, India',
+        placeId: 'ChIJW3Or_8dHrjsROESo0OikUl4'
     },
     {
-        title: '',
-        position: {lat: '', lng: ''}
+        title: 'ISKCON Temple',
+        position: {lat: 13.0096323, lng: 77.55107099999999},
+        address: 'Hare Krishna Hill, Chord Road, Rajaji Nagar, Bengaluru, Karnataka 560010, India',
+        placeId: 'ChIJBw42C-09rjsRs7KmQUqyf3o'
     },
+    
 ];
+
+/* HTTP geolocaitn api to find the address of the particular area */
+//https://maps.googleapis.com/maps/api/geocode/json?address=ISKCON+Bangalore&key=AIzaSyA31u0Hxmq37sPOLezIMM8wg0VLJd5E0Sg
