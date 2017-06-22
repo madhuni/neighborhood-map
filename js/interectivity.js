eb4513a8c4b8949217e2dca5edf4b541da13a5d6
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-06-23 01:58:09 +0530

    Added new js file for separation of all the interectivity taken place in the project.

---
+++ js/interectivity.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById('navigation-container').style.width='300px';
+    document.getElementById('wrapper').style.marginLeft='300px';
+};
+
+function closeNav() {
+    document.getElementById('navigation-container').style.width='0px';
+    document.getElementById('wrapper').style.marginLeft='0px';
+}
