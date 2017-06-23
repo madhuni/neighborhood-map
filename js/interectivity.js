ce3c2a067d60b3f5b87633dad56ec6bb3e12f5cb
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-06-23 11:32:13 +0530

    Adding the padding to the navigation panel once the button is clicked to open the panel. While closing the panel, the padding has been removed from the panel.

--- js/interectivity.js
+++ js/interectivity.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById('navigation-container').style.width='300px';
+    document.getElementById('navigation-container').style.padding='10px';
     document.getElementById('wrapper').style.marginLeft='300px';
 };
 
 function closeNav() {
     document.getElementById('navigation-container').style.width='0px';
+    document.getElementById('navigation-container').style.padding='0px';
     document.getElementById('wrapper').style.marginLeft='0px';
 }
