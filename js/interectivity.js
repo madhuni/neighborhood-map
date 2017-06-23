c2d21cb7499cf4400e95d6c54ba139e7791d54d0
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-06-23 21:35:07 +0530

    Added jQuery code to handle the opening and closing of the navigation panel

--- js/interectivity.js
+++ js/interectivity.js
@@ -1,3 +1,4 @@
+/* Below functions were used when pure javascript was used */
 function openNav() {
     document.getElementById('navigation-container').style.width='300px';
     document.getElementById('wrapper').style.marginLeft='300px';
@@ -7,3 +8,17 @@ function closeNav() {
     document.getElementById('navigation-container').style.width='0px';
     document.getElementById('wrapper').style.marginLeft='0px';
 }
+
+$(function () {
+    /* Using jQuery to toggle the open and close the navigation panel */
+    var hamburger = $('.fa-bars');
+    var wrapper = $('#wrapper');
+    var nav = $('#navigation-container');
+    
+    hamburger.on('click', function () {
+        console.log("I am working fine");
+        nav.toggleClass('open-nav');
+        wrapper.toggleClass('wrapper-off');
+    });
+    
+});
