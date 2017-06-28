b8d560625610ad427b50d4eead8c361736b2cdb3
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-06-28 10:57:37 +0530

    Added sample code from the discussion with mentor

--- supporting_material/snippet_app.js
+++ supporting_material/snippet_app.js
@@ -21,3 +21,91 @@
 
 // Apply the bindings aka activate KO
 // http://knockoutjs.com/documentation/observables.html#mvvm-and-view-models#activating-knockout 
+
+/* snippet from the discussion on 27th June */
+
+var map;
+var markers = [];
+
+var initMap = function () {
+
+    vm.google(!!window.google); // true
+    var mapOptions = {
+        center: {lat: 12.9715987,lng: 77.5945627},
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    };
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < locations.length; i++) {
+        var title = locations[i].title;
+        var position = locations[i].position;
+        var marker = new google.maps.Marker({
+            map: map,
+            title: title,
+            position: position,
+            draggable: false,
+            animation: google.maps.Animation.DROP
+        });
+        markers.push(marker);
+        vm.locationContainer()[i].marker = marker;
+
+        // make markers clickable here, so that an info window opens
+
+        bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
+};
+
+/* Starting the code for Project */
+
+/* Initializing the Location constructor */
+var Location = function (data) {
+    this.title = ko.observable(data.title);
+    this.position = data.position;
+    this.address = ko.observable(data.address);
+    this.placeId = ko.observable(data.placeId);
+
+    // don't
+    // this.marker = ko.observable(new google.maps.Marker({}))
+
+    // when the google maps api has finished loading
+    //this.marker = google.maps.Marker({map: map,})
+};
+
+var ViewModel = function () {
+    var self = this;
+    
+    self.locationContainer = ko.observableArray([]);
+    
+    for (var i = 0; i < locations.length; i++) {
+        self.locationContainer.push(new Location(locations[i]));
+    }
+
+    self.activateMarker = function(location) {
+        console.log(location);
+        // do something with location.marker
+        // for example: google.maps.event.trigger() method
+
+    };
+ 
+    self.google = ko.observable(!!window.google); // false
+
+    // we can only execute script which depends on the google maps api when the google maps api has finished loading
+    self.googleFinishedLoading = ko.computed(function() {
+        console.log(self.google()); // false the first time this function executes
+         // google.maps.Marker({}); error google is not defined
+
+        if (self.google()) {
+            console.log("The Google Maps API has finished loading. Let's create, for example the marker objects");
+            console.log(google);
+
+            // google.maps.Marker({}); no error
+        }
+
+    });
+};
+
+var vm = new ViewModel();
+
+ko.applyBindings(vm);
