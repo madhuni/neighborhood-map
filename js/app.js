88cb20ff48afce1bc868f8c96b6c6d99e07bcf44
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-06-29 21:33:05 +0530

    1. Makers have been added as property of the Location object. 2. The ViewModel object is initialized within the 'initMap' function. 3. Proper comments have been added.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,54 @@
-var map;
-var markers = [];
+/* Defining the Global Variables */
+var map, largeInfoWindow;
+
+/* Defining the initMap call back function */
 var initMap = function () {
     var mapOptions = {
         center: {lat: 12.9715987,lng: 77.5945627},
         zoom: 13,
-        mapTypeId: 'roadmap'
+        mapTypeId: 'roadmap',
+        scrollwheel: false,
+        scaleControl: false,
+        rotateControl: false,
+        zoomControl: false
     };
+    
+    /* Initializing the map object */
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+    /* Initializing the bounds object */
     var bounds = new google.maps.LatLngBounds();
-    for (var i = 0; i < locations.length; i++) {
-        var title = locations[i].title;
-        var position = locations[i].position;
-        var marker = new google.maps.Marker({
-            map: map,
-            title: title,
-            position: position,
-            draggable: false,
-            animation: google.maps.Animation.DROP
-        });
-        markers.push(marker);
-        bounds.extend(markers[i].position);
+    
+    /* Initializing the viewModel object */
+    var vm = new ViewModel();
+    var locContainer = vm.locationContainer();
+    
+    for (var i = 0; i< locContainer.length; i++) {
+        bounds.extend(locContainer[i].marker.position);
     }
+    
     map.fitBounds(bounds);
+    
+    /* Applying the bindings */
+    ko.applyBindings(vm);
 };
 
-/* Starting the code for Project */
-
-/* Initializing the Location constructor */
+/* Defining the Location constructor */
 var Location = function (data) {
     this.title = ko.observable(data.title);
-    this.position = data.position;
+    this.position = ko.observable(data.position);
     this.address = ko.observable(data.address);
     this.placeId = ko.observable(data.placeId);
+    this.marker = new google.maps.Marker({
+        map: map,
+        title: this.title(),
+        position: this.position(),
+        draggable: false,
+        animation: google.maps.Animation.DROP
+    });
 };
 
+/* Defining the ViewModel constructor */
 var ViewModel = function () {
     var self = this;
     
@@ -44,6 +59,3 @@ var ViewModel = function () {
     }
 };
 
-var vm = new ViewModel();
-
-ko.applyBindings(vm);
