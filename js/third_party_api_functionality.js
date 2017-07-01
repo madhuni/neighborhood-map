bd8bd7dbf58b78964102d063077398d547fb182f
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-07-01 19:17:04 +0530

    Modified the code to capture the error details and also the marker object is added to fetch the data

--- js/third_party_api_functionality.js
+++ js/third_party_api_functionality.js
@@ -1,12 +1,16 @@
-function getContentFromFoursquare () {
+function getContentFromFoursquare (marker) {
+    var venueCordinates = marker.position;
+    // console.log(venueCordinates);
+    // console.log(marker.title);
+    // console.log(venueCordinates.lat() + " and " + venueCordinates.lng());
 	var clientId = "UPSN54SKPJL2AIQ4QL55UDZADYQKDP545LTWKAZAUN5MVGLN";
 	var clientSecret = "ZPRUPW2TZYLW4SIE41RXUZMKJYCD3TE25EPNMP3305ZOCYTK";
 	var url = "https://api.foursquare.com/v2/venues/suggestcompletion";
 	url += "?" + $.param({
         client_id: clientId,
         client_secret: clientSecret,
-        ll: "12.997572" + ',' + "77.69633899999999",
-        query: "Phoenix",
+        ll: venueCordinates.lat() + ',' + venueCordinates.lng(),
+        query: marker.title,
         v: "20170601",
         m: "foursquare"
     });
@@ -15,40 +19,49 @@ function getContentFromFoursquare () {
         url: url,
         dataType: 'json',
         success: function (data) {
-            //console.log(url);
-            //console.log(data);
-            var venue = data.response.minivenues[0];
-            var venueUrl = "https://api.foursquare.com/v2/venues/" + venue.id + "/tips";
-            venueUrl += "?" + $.param({
-                client_id: clientId,
-                client_secret: clientSecret,
-                v: "20170601",
-                m: "foursquare",
-                sort: "recent",
-                limit: "100"
-            });
-            
-            $.ajax({
-                url: venueUrl,
-                dataType: "json",
-                success: function (data) {
-                    //console.log(data);
-                    var tips = data.response.tips.items;
-                    for (var i = 0; i < tips.length; i++) {
-                        console.log(tips[i].text);
+            console.log("request is successful");
+            // console.log(url);
+            // console.log(data);
+            if (data.response.minivenues.length !== 0) {
+                var venue = data.response.minivenues[0];
+                var venueUrl = "https://api.foursquare.com/v2/venues/" + venue.id + "/tips";
+                venueUrl += "?" + $.param({
+                    client_id: clientId,
+                    client_secret: clientSecret,
+                    v: "20170601",
+                    m: "foursquare",
+                    sort: "recent",
+                    limit: "100"
+                });
+                
+                $.ajax({
+                    url: venueUrl,
+                    dataType: "json",
+                    success: function (data) {
+                        // console.log(data);
+                        if (data.response.tips.count !== 0) {
+                            var tips = data.response.tips.items;
+                            for (var i = 0; i < tips.length; i++) {
+                                console.log(tips[i].text);
+                            }    
+                        } else {
+                            console.log("no tips are found for the location");
+                        }
+                    },
+                    error: function () {
+                        console.log("Tips are not loaded");
                     }
-                },
-                error: function () {
-                    console.log("Tips are not loaded");
-                }
-            });
+                });    
+            } else {
+                console.log("No venues were found for the provied location query");
+            }
+            
         },
-        error: function () {
-            console.log("Request didn't work");
+        error: function (data) {
+            var responseObject = JSON.parse(data.responseText).meta;
+            console.log("code : " + responseObject.code);
+            console.log("error msg : " + responseObject.errorDetail);
+            console.log("error type: " + responseObject.errorType);
         }
     });
 };
-
-getContentFromFoursquare();
-
-
