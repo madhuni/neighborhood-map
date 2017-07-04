892b55456bb4b0d35dc2ad5f84c6b34abd479d8f
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-07-05 03:42:27 +0530

    weather API is chaged to work with jQuery promise modifications are going on for making the ajax call aysync

--- js/third_party_api_functionality.js
+++ js/third_party_api_functionality.js
@@ -1,4 +1,5 @@
 function getContentFromFoursquare (marker) {
+    var infoContent;
     var venueCordinates = marker.position;
     // console.log(venueCordinates);
     // console.log(marker.title);
@@ -25,15 +26,17 @@ function getContentFromFoursquare (marker) {
             if (data.response.minivenues.length !== 0) {
                 var venue = data.response.minivenues[0];
                 var venueId = venue.id;
-                // getVenueTips(venueId, clientId, clientSecret);
+                
+                getVenueTips(venueId, clientId, clientSecret, marker);
+                // debugger;
+                // console.log(infoContent);
+                // return infoContent;
                 // getNextVenues(venueId, clientId, clientSecret);
                 // getVenuePhotoes(venueId, clientId, clientSecret);
                 // getSimilarVenues(venueId, clientId, clientSecret);
-
             } else {
                 console.log("No venues were found for the provided location query");
             }
-            
         },
         error: function (data) {
             var responseObject = JSON.parse(data.responseText).meta;
@@ -45,7 +48,9 @@ function getContentFromFoursquare (marker) {
 };
 
 /* Function to retrive the tips about the places from Foursquare */
-function getVenueTips (venueId, clientId, clientSecret) {
+function getVenueTips (venueId, clientId, clientSecret, marker) {
+    var content = "";
+    var tipsArray = [];
     var venueUrl = "https://api.foursquare.com/v2/venues/" + venueId + "/tips";
     venueUrl += "?" + $.param({
         client_id: clientId,
@@ -56,24 +61,60 @@ function getVenueTips (venueId, clientId, clientSecret) {
         limit: "10"
     });
     
-    $.ajax({
+//    $.ajax({
+//        url: venueUrl,
+//        dataType: "json",
+//        success: function (data) {
+//            // console.log(data);
+//            if (data.response.tips.count !== 0) {
+//                var tips = data.response.tips.items;
+//                for (var i = 0; i < tips.length; i++) {
+//                    // console.log(tips[i].text);
+//                    tipsArray.push('<li>' + tips[i].text + '</li>');
+//                }
+//                // console.log(tipsArray);
+//                content += '<h2 class="info-header">' + marker.title.toUpperCase() + '</h2>' + '<h3 class="info-sub-header">Most Recent Comments</h3>' + '<ol class="tips">' + tipsArray.join('') + '</ol>';
+//                // console.log(content);
+//            } else {
+//                console.log("no tips are found for the location");
+//            }
+//             console.log(content);
+//            // debugger;
+//            return content;
+//        },
+//        error: function () {
+//            
+//        }
+//    });
+    
+    var getVenueTips = $.ajax({
         url: venueUrl,
-        dataType: "json",
-        success: function (data) {
-            // console.log(data);
-            if (data.response.tips.count !== 0) {
-                var tips = data.response.tips.items;
-                for (var i = 0; i < tips.length; i++) {
-                    // console.log(tips[i].text);
-                }    
-            } else {
-                console.log("no tips are found for the location");
-            }
-        },
-        error: function () {
-            console.log("Tips are not loaded");
-        }
+        dataType: 'json'
     });
+    
+    getVenueTips.done(function (data) {
+        if (data.response.tips.count !== 0) {
+            var tips = data.response.tips.items;
+            for (var i = 0; i < tips.length; i++) {
+                // console.log(tips[i].text);
+                tipsArray.push('<li>' + tips[i].text + '</li>');
+            }
+            // console.log(tipsArray);
+            this.content += '<h2 class="info-header">' + marker.title.toUpperCase() + '</h2>' + '<h3 class="info-sub-header">Most Recent Comments</h3>' + '<ol class="tips">' + tipsArray.join('') + '</ol>';
+            // console.log(content);
+        } else {
+            console.log("no tips are found for the location");
+        }
+          console.log(this.content);
+        // debugger;
+    });
+    
+    getVenueTips.fail(function () {
+        console.log("Tips are not loaded");
+    });
+    
+    console.log(content);
+    return content;
 };
 
 /* Function to retrive the hours of the venue*/
@@ -172,39 +213,40 @@ function getSimilarVenues (venueId, clientId, clientSecret) {
 /* Function to retrive weather for the city */
 function getWeather () {
     var apiKey = "a71fa89ba24601d8940995e04f9d6bb6";
-    var weatherUrl = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather";
+    var weatherUrl = "http://api.openweathermap.org/data/2.5/weather";
     var cityId = "1277333";
     weatherUrl += "?" + $.param({
         id: cityId,
         APPID: apiKey,
         units: "metric"
     });
-    
-    $.ajax({
+
+    var weatherPromise = $.ajax({
         url: weatherUrl,
-        dataType: "json",
-        success: function (data) {
-            console.log("weather is fetched");
-            // console.log(data);
-            var currentTemp = data.main.temp;
-            var maxTemp = data.main.temp_max;
-            var minTemp = data.main.temp_min;
-            var humidity = data.main.humidity;
-            var weather = data.weather[0].description;
-            console.log("Temperature is : " + currentTemp + 
-                "\n Weather : " + weather + 
-                "\n Humidity : " + humidity + "%"
-                );
-            
-            /* Adding live data to the dom elements */
-            $(".temp").text(currentTemp);
-            $(".weather").text(weather);
-            $(".humidity").text(humidity+"%");
-            
-        },
-        error: function () {
-            console.log("Oops...Something went wrong !!! :( ");
-        }
+        dataType: 'json'
+    });
+    
+    weatherPromise.done(function (data) {
+        // console.log("weather is fetched");
+        // console.log(data);
+        var currentTemp = data.main.temp;
+        var maxTemp = data.main.temp_max;
+        var minTemp = data.main.temp_min;
+        var humidity = data.main.humidity;
+        var weather = data.weather[0].description;
+        console.log("Temperature is : " + currentTemp + 
+            "\n Weather : " + weather + 
+            "\n Humidity : " + humidity + "%"
+            );
+        
+        /* Adding live data to the dom elements */
+        $(".temp").text(currentTemp);
+        $(".weather").text(weather);
+        $(".humidity").text(humidity+"%");
+    });
+    
+    weatherPromise.fail(function (e) {
+        console.log("Oops...Something went wrong !!! :( ");
     });
 };
 
